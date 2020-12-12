a787a3d092f23007f286ea5d655a1a176836d803
Muhammad Idrees <midexigner@gmail.com>
2020-12-13 03:54:21 +0500

    complete portfolio site

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
 import React,{useState,useEffect} from 'react'
 import sanityClient from '../client.js'
+import plumeria from '../palm-leaf.jpg'
+import imageUrlBuilder from '@sanity/image-url'
+import BlockContent from '@sanity/block-content-to-react'
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source){
+    return builder.image(source)
+}
 
 export default function About() {
     const [author,setAuthor] = useState(null);
@@ -7,8 +14,7 @@ export default function About() {
         sanityClient.fetch(`*[_type == "author"]{
             name,
             bio,
-           "authorImage:image.asset->url
-            
+           "authorImage":image.asset->url
         }`).then((data) => setAuthor(data[0]))
         .catch(console.error)
     }, []);
@@ -17,14 +23,20 @@ export default function About() {
 
     return (
         <main className="relative">
-            <img src="" alt=""/>
-            <div>
-                <section>
-                    <img src="" alt=""/>
-                    <div>
-                        <h1></h1>
-                        <div>
-                            Block COntent
+            <img src={plumeria} alt="plumeria Flower" className="absolute w-full"/>
+            <div className="p-10 lg:pt-48 container mx-auto relative">
+                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt=""/>
+                    <div className="text-lg flex flex-col justify-center">
+                        <h1 className="cursive text-6xl text-green-300 mb-4">
+                        Hey there. I'm{" "}<span class="text-green-100">{author.name}</span>!
+                        </h1>
+                        <div className="prose lg:prose-xl text-white">
+                        <BlockContent
+                  blocks={author.bio} 
+                  projectId="o21v7dzg" 
+                  dataset="production"
+                   />
                         </div>
                     </div>
                 </section>
